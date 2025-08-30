bc8cdc459cea003b81bd36e1323b41e204063fd4
Ee Szen <eeszenlee@gmail.com>
2025-08-30 20:42:33 +0800

    base addFragment with viewModel extended from BaseManageWordFragment

--- app/src/main/java/com/dx/wordapp/ui/manage/AddWordFragment.kt
+++ app/src/main/java/com/dx/wordapp/ui/manage/AddWordFragment.kt
@@ -1,20 +1,48 @@
 package com.dx.wordapp.ui.manage
 
+import android.annotation.SuppressLint
 import androidx.fragment.app.viewModels
 import android.os.Bundle
-import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
 import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.setFragmentResult
+import androidx.lifecycle.lifecycleScope
+import androidx.navigation.fragment.findNavController
 import com.dx.wordapp.R
-import com.dx.wordapp.databinding.FragmentBaseManageWordBinding
+import kotlinx.coroutines.launch
 
 class AddWordFragment : BaseManageWordFragment() {
     private val viewModel: AddWordViewModel by viewModels()
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        binding.run {
+            mbSubmit.text = getString(R.string.add)
+            mbSubmit.setOnClickListener {
+                val title = etTitle.text.toString()
+                val definition = etDefinition.text.toString()
+                val synonym = etSynonym.text.toString()
+                val details = etDetails.text.toString()
+                viewModel.addWord(title = title,definition = definition, synonym = synonym, details = details)
+            }
+        }
+        finish()
+        error()
+    }
 
-        binding
+    fun finish(){
+        lifecycleScope.launch {
+            viewModel.finish.collect{
+                setFragmentResult("manage_word",Bundle())
+                findNavController().popBackStack()
+            }
+        }
+    }
+
+    fun error(){
+        lifecycleScope.launch {
+            viewModel.error.collect{
+                showError(it)
+            }
+        }
     }
 }
