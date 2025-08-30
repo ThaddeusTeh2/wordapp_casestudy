02c1ed37f71af467cc6948ce6ef03163db39b738
Ee Szen <eeszenlee@gmail.com>
2025-08-30 20:45:38 +0800

    BaseFragment for add and edit word with a showError function to show errors if any errors

--- app/src/main/java/com/dx/wordapp/ui/manage/BaseManageWordFragment.kt
+++ app/src/main/java/com/dx/wordapp/ui/manage/BaseManageWordFragment.kt
@@ -5,9 +5,13 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import androidx.fragment.app.setFragmentResult
+import androidx.navigation.fragment.findNavController
 import com.dx.wordapp.R
 import com.dx.wordapp.data.repo.WordsRepo
 import com.dx.wordapp.databinding.FragmentBaseManageWordBinding
+import com.google.android.material.snackbar.Snackbar
 
 open class BaseManageWordFragment : Fragment() {
     protected val repo = WordsRepo.getInstance()
@@ -22,5 +26,10 @@ open class BaseManageWordFragment : Fragment() {
         return binding.root
     }
 
-    // navigationBack function here (waiting for navGraph)
+    // Show error function
+    fun showError(msg:String){
+        val snackbar = Snackbar.make(binding.root,msg, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
+        snackbar.show()
+    }
 }
