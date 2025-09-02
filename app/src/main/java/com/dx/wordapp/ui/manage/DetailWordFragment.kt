795dfa293a3ca9df1b454ee97cd1d4a95b9a18a1
Ee Szen <eeszenlee@gmail.com>
2025-09-02 17:10:07 +0800

    Finished the details fragment except for btnDone(handle it later)

--- app/src/main/java/com/dx/wordapp/ui/manage/DetailWordFragment.kt
+++ app/src/main/java/com/dx/wordapp/ui/manage/DetailWordFragment.kt
@@ -5,17 +5,46 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.fragment.navArgs
 import com.dx.wordapp.R
+import com.dx.wordapp.data.repo.WordsRepo
+import com.dx.wordapp.databinding.FragmentDetailWordBinding
 
 
-class DetailWordFragment : Fragment() {
+class DetailWordFragment: Fragment() {
+    protected val repo = WordsRepo.getInstance()
+    private lateinit var binding : FragmentDetailWordBinding
+    private val args : DetailWordFragmentArgs by navArgs()
 
-        override fun onCreateView(
+    override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_detail_word, container, false)
+        binding = FragmentDetailWordBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        val word = repo.getWord(args.wordId) ?: throw Exception("Word is Null")
+        binding.run {
+            tvTitle.setText(word.title)
+            tvDefinition.setText(word.definition)
+            tvSynonym.setText(word.synonym)
+            tvDetails.setText(word.details)
+
+//            btnDone.setOnClickListener{}
+            btnEdit.setOnClickListener {
+                val action = DetailWordFragmentDirections.actionDetailWordFragmentToEditWordFragment(it.id!!)
+                findNavController().navigate(action)
+            }
+            btnDelete.setOnClickListener {
+                repo.deleteWord(it.id!!)
+            }
+        }
     }
 
 
