e1e9a89a59b1991fbec586c50c84b3749e43d173
Ee Szen <eeszenlee@gmail.com>
2025-09-07 04:57:31 +0800

    BaseHomeFragment that renders the UI of home and completed page with a new searchbar implementation to show searchings

---
+++ app/src/main/java/com/dx/wordapp/ui/home/BaseHomeFragment.kt
@@ -0,0 +1,53 @@
+package com.dx.wordapp.ui.home
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.core.content.ContextCompat
+import com.dx.wordapp.R
+import com.dx.wordapp.databinding.FragmentBaseHomeBinding
+import com.google.android.material.search.SearchBar
+import com.google.android.material.search.SearchView
+import com.google.android.material.snackbar.Snackbar
+
+class BaseHomeFragment : Fragment() {
+    protected lateinit var binding: FragmentBaseHomeBinding
+
+    private lateinit var searchBar: SearchBar
+    private lateinit var searchView: SearchView
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        // Inflate the layout for this fragment
+        binding = FragmentBaseHomeBinding.inflate(inflater,container,false)
+        return binding.root
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        searchBar = view.findViewById(R.id.search)
+        searchView = view.findViewById(R.id.search_view)
+
+        // Link SearchView with SearchBar
+        searchView.setupWithSearchBar(searchBar)
+
+        // Listen for text changes
+//        searchView.editText.addTextChangedListener { text ->
+//            // filter your RecyclerView adapter here
+//            // example: adapter.filter(text.toString())
+//        }
+    }
+
+
+    fun showError(msg:String){
+        val snackbar = Snackbar.make(binding.root,msg, Snackbar.LENGTH_LONG)
+        snackbar.setBackgroundTint(ContextCompat.getColor(requireContext(), R.color.red))
+        snackbar.show()
+    }
+
+}
