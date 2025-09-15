555a6c988a60f1d5f56ea289a3074e6726f02aac
Ee Szen <eeszenlee@gmail.com>
2025-09-15 21:10:14 +0800

    Define Entity and Primary Key

--- app/src/main/java/com/dx/wordapp/data/model/Word.kt
+++ app/src/main/java/com/dx/wordapp/data/model/Word.kt
@@ -1,10 +1,14 @@
 package com.dx.wordapp.data.model
 
+import androidx.room.Entity
+import androidx.room.PrimaryKey
 import java.time.LocalDateTime
 
 // Data class for Word (data)
 
+@Entity
 data class Word (
+    @PrimaryKey(autoGenerate = true)
     val id : Int? = null,
     val title : String,
     val definition : String,
